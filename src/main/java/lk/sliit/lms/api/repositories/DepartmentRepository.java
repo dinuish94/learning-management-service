da9d9ba2dc4c4ed64ebf4cfe01c2596f21b586e9
Dinuksha Ishwari <dinukshai@zone24x7.com>
2017-09-26 07:47:23 +0530

    Add department repository

---
+++ src/main/java/lk/sliit/lms/api/repositories/DepartmentRepository.java
@@ -0,0 +1,10 @@
+package lk.sliit.lms.api.repositories;
+
+import lk.sliit.lms.api.models.Department;
+import org.springframework.data.repository.CrudRepository;
+
+/**
+ * Created by dinukshakandasamanage on 9/25/17.
+ */
+public interface DepartmentRepository extends CrudRepository<Department,Long> {
+}
