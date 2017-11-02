5c7812b8645f9337cde284843fb02288a15fbdef
Jonathan <jonathandeon.k@gmail.com>
2017-11-02 08:01:34 +0530

    Added Feedback session create and retrieval

---
+++ src/main/java/lk/sliit/lms/api/repositories/FeedBackRepository.java
@@ -0,0 +1,10 @@
+package lk.sliit.lms.api.repositories;
+
+import lk.sliit.lms.api.models.FeedBack;
+import org.springframework.data.repository.CrudRepository;
+
+/**
+ * Created by Jonathan on 11/1/2017.
+ */
+public interface FeedBackRepository extends CrudRepository<FeedBack, Long> {
+}
