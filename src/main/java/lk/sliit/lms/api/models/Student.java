0b1f4aa7d58a6cf6961c25d29ac33aa77cf184d2
Kashif Roshen <kashifroshen@MacBooks-MacBook-Pro.local>
2017-11-03 11:56:02 +0530

    student email and constructor added

--- src/main/java/lk/sliit/lms/api/models/Student.java
+++ src/main/java/lk/sliit/lms/api/models/Student.java
@@ -21,6 +21,15 @@ public class Student{
     private Long sId;
 
     private String name;
+    private String email;
+
+    public String getEmail() {
+        return email;
+    }
+
+    public void setEmail(String email) {
+        this.email = email;
+    }
 
     @ManyToMany(mappedBy = "students")
     private Set<Course> courses;
@@ -64,10 +73,18 @@ public class Student{
         this.studentAssignment.add(studentAssignment);
     }
 
+    public Student(String name, String email, Set<Course> courses, Set<StudentAssignment> studentAssignment) {
+        this.name = name;
+        this.email = email;
+        this.courses = courses;
+        this.studentAssignment = studentAssignment;
+    }
+
     @Override
     public String toString() {
         return "Student{" +
                 "sId=" + sId +
+
                 ", name='" + name + '\'' +
                 '}';
     }
